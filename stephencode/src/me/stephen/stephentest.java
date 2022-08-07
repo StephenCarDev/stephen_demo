3a75d6c8509e66e0768a5c47765d53bf2c9176f5
MATEBOOKXS\zhanf\SourceTree <zhanfeng990818@126.com>
2022-08-07 11:21:09 +0800

    festure：测试修改

--- stephencode/src/me/stephen/stephentest.java
+++ stephencode/src/me/stephen/stephentest.java
@@ -3,5 +3,6 @@ package me.stephen;
 public class stephentest {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test002222");
     }
 }
